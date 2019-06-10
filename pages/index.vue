ec94381e8311ce57291e2f0ad58f126c03dbf6d4
Colin Travis <colint@cybertravis.us>
2019-06-09 19:55:18 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-grey-lighter h-screen font-stolzl">
+    <div class="container mx-auto h-full flex justify-center items-center">
+      <div class="w-auto sm:w-auto lg:w-1/3 px-4">
+        <div class="border-teal p-4 border-t-12 bg-white mb-6 rounded-lg shadow-xl">
+          <logo class="mx-auto block w-1/2"/>
+          <h1 class="text-center mb-8">ColinTravis.com</h1>
+          <div class="flex flex-col items-center justify-between">
+            <h1 class="text-center font-medium text-xl mb-4">Exciting new things are coming.</h1>
+            <p class="text-center font-light"> A new build of this website based on Nuxt.js, with a blazing fast headless CMS is prepping to deploy...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+// See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+module.exports = {
+  theme: {
+
+
+    fontFamily: {
+      stolzl: [
+        'Stolzl',
+      ],
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: [
+        'Georgia',
+        'Cambria',
+        '"Times New Roman"',
+        'Times',
+        'serif',
+      ],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
+
+    extend: {}
+  },
+  variants: {}
+}
