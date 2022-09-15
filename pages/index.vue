5a7d4dd2de8097c6b30bf12b1b9ce4a6dc9ff39b
ColinTravis <colint@cybertravis.us>
2022-09-15 14:43:47 -0500

    Adding github link

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="bg-grey-lighter h-screen font-stolzl">
-    <div class="container mx-auto h-full flex justify-center items-center">
-      <div class="w-auto sm:w-auto lg:w-1/3 px-4">
-        <div class="border-teal p-4 border-t-12 bg-white mb-6 rounded-lg shadow-xl">
-          <logo class="mx-auto block w-1/2"/>
-          <h1 class="text-center mb-8">ColinTravis.com</h1>
-          <p class="text-center">🤷‍♂️ ⏳</p>
-          <!-- <div class="flex flex-col items-center justify-between">
-            <h1 class="text-center font-medium text-xl mb-4">Exciting new things are coming.</h1>
-            <p class="text-center font-light"> A new build of this website based on Nuxt.js, with a blazing fast headless CMS is prepping to deploy...</p>
-          </div> -->
+  <div class="h-screen bg-grey-lighter font-stolzl">
+    <div class="container flex items-center justify-center h-full mx-auto">
+      <div class="w-auto px-4 sm:w-auto lg:w-1/3">
+        <div
+          class="flex flex-col p-4 mb-6 bg-white rounded-lg shadow-xl border-teal border-t-12"
+        >
+          <logo class="block w-1/2 mx-auto" />
+          <h1 class="mb-5 text-center">ColinTravis.com</h1>
+          <p class="mb-5 text-center">🤷‍♂️ ⏳</p>
+          <a class="mx-auto" href="https://github.com/ColinTravis">
+            <IconGithub class="w-8 h-8 hover:text-ct-blue git-icon" />
+          </a>
         </div>
       </div>
     </div>
@@ -18,10 +19,20 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
+import IconGithub from '~/components/icons/Icon-Github.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    IconGithub
   }
 }
 </script>
+
+<style>
+.git-icon {
+  transition-property: color;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 200ms;
+}
+</style>
